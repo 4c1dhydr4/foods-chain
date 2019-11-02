9993f7eefe8b4962faae13d035948bee4f1d7db5
Luis Quiroz <4c1dhydr4@gmail.com>
2019-11-02 13:58:47 -0500

    v0.1

--- blockchain/settings.py
+++ blockchain/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'q*l@n0#z=3e(bt2y@*+)9r&)jq52yj@4*!673#g%#04uy*^z3t'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
