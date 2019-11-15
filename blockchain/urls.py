ac5548d87cf3d856534bdf0e7b91eebdbfc98197
Luis Augusto Quiroz Burga <4c1dhydr4@gmail.com>
2019-11-15 12:33:27 -0500

    camera

--- blockchain/urls.py
+++ blockchain/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 from views.dashboard import (index,tacks,blockchain_card, traceability)
+from views.phone import (phone,)
 
 urlpatterns = [
-	path('', index, name="index"),
-	path('tacks/', tacks, name="tacks"),
-	path('blockchain_card/', blockchain_card, name="blockchain_card"),
-	path('traceability/', traceability, name="traceability"),
+	path('', login_required(index), name="index"),
+	path('phone/', login_required(phone), name="phone"),
+	path('tacks/', login_required(tacks), name="tacks"),
+	path('blockchain_card/', login_required(blockchain_card), name="blockchain_card"),
+	path('traceability/', login_required(traceability), name="traceability"),
     path('admin/', admin.site.urls),
+	path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+	path('accounts/logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
---
+++ views/phone.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+def phone(request):
+	return render(request, 'phone.html', {})
