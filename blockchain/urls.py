ffb8ea4fb6476e1cae30919572b71459eb2a431a
Luis Augusto Quiroz Burga <4c1dhydr4@gmail.com>
2019-11-15 14:35:35 -0500

    camera

--- blockchain/urls.py
+++ blockchain/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
 from views.dashboard import (index,tacks,blockchain_card, traceability)
-from views.phone import (phone,)
+from views.phone import (phone, cell_traceability,)
 
 urlpatterns = [
 	path('', login_required(index), name="index"),
 	path('phone/', login_required(phone), name="phone"),
+	path('cell_traceability/', login_required(cell_traceability), name="cell_traceability"),
 	path('tacks/', login_required(tacks), name="tacks"),
 	path('blockchain_card/', login_required(blockchain_card), name="blockchain_card"),
 	path('traceability/', login_required(traceability), name="traceability"),
--- views/phone.py
+++ views/phone.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 def phone(request):
 	return render(request, 'phone.html', {})
+
+def cell_traceability(request):
+	return render(request, 'cell_traceability.html', {})
