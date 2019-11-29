07436407c2a9fcfb7496a0998d5e1366022f6c48
Luis Augusto Quiroz Burga <4c1dhydr4@gmail.com>
2019-11-29 11:52:15 -0500

    cases 1

--- views/cases.py
+++ views/cases.py
@@ -3,4 +3,11 @@ from datetime import datetime
 
 def salida_almacen(request):
 	now = datetime.today()
-	return render(request, 'case/salida_almacen.html', {'now':now})
+	tomo = "{}/{}/{} {}:{}".format(
+			now.day + 1,
+			now.month,
+			now.year,
+			now.hour + 2,
+			now.minute + 5,
+		)
+	return render(request, 'case/salida_almacen.html', {'now':now, 'tomo':tomo})
